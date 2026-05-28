34b41bdce32b0e52b0e6923119e8bff82b6bda0d
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-28 09:25:56 +0200

    [refactor] - Extraidas 3 funciones privadas de las condiciones de la clase principal

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,15 +6,15 @@ class FizzBuzz
 {
     public function evaluate(string $number):string{
 
-        if(((intval($number) % 3) == 0) and ((intval($number) % 5) == 0)){
+        if($this->divisibleBy3and5($number)){
             return "FizzBuzz";
         }
 
-        if((intval($number) % 3) == 0){
+        if($this->divisibleBy3($number)){
             return "Fizz";
         }
 
-        if((intval($number) % 5) == 0){
+        if($this->divisibleBy5($number)){
             return "Buzz";
         }
 
@@ -23,4 +23,15 @@ class FizzBuzz
         }
     }
 
+    private function divisibleBy3and5(string $number):bool{
+        return (((intval($number) % 3) == 0) and ((intval($number) % 5) == 0));
+    }
+
+    private function divisibleBy3(string $number):bool{
+        return ((intval($number) % 3) == 0);
+    }
+
+    private function divisibleBy5(string $number):bool{
+        return ((intval($number) % 5) == 0);
+    }
 }
