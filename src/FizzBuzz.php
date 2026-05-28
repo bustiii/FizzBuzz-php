21f0f6c545aec280c90bd281977c684fb1915356
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-28 08:52:16 +0200

    [verde] - Creado test en verde para recibir un numero normal

---
+++ src/FizzBuzz.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Deg540\CleanCodeKata9;
+
+class FizzBuzz
+{
+    public function evaluate():string{
+        return "1";
+    }
+
+}
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Deg540\CleanCodeKata9\Test;
 
-use Deg540\CleanCodeKata9\Example;
+use Deg540\CleanCodeKata9\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
-final class ExampleTest extends TestCase
+final class FizzBuzzTest extends TestCase
 {
 
     /**
